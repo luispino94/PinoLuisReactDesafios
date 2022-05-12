33a958805cee8391fefc706dc841c64d4524aefa
luispino94 <luispinito.torres@gmail.com>
2022-05-11 21:55:22 -0300

    componente item agregado

--- React-eCommerce/src/componentes/Items/Item.jsx
+++ React-eCommerce/src/componentes/Items/Item.jsx
@@ -1,13 +1,53 @@
-// import { productList } from '../../datos/datos.js';
+import { useState, useEffect } from 'react';
+import { productList } from '../../datos/datos.js';
+import './itemlist.scss'
 
-const Item = ({name,price,skin, id})=> {
+const getFetch = new Promise ((resolve)=>{
+    setTimeout(() => {
+        
+        resolve (productList);
+    }, 4000);
+})
+
+
+const Item = ()=> {
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState (true)
+
+  /*Con useEffect + los corchetes hacemos que cargue una sola vez y en segundo plano ( o sea, un array, se ejecuta una sola vez
+    )  */ 
+    useEffect(() => { 
+       getFetch 
+       .then (respuesta => setProductos (respuesta))
+       .catch ((err)=> console.log (err))
+       .finally(()=>setLoading (false)) 
+     }, []); /*Los corchetes van acá*/ 
     
+
+
     return (
        <section>
-               <img className="product-card__image" src={skin} alt="" />
-              
-              <h3 className="product-card__name">{name}</h3>               
-              <span className="product-card__name">${price}</span>
+           { loading ?
+           <h2>cargando...</h2>
+           :
+           productos.map((prod)=><div key={prod.id}
+                                    className = "container-section"    
+                                    >
+                                    <div className='container-items'>
+                                        <div className='container-titulo'>
+                                        {`${prod.name}`}
+                                        </div>
+                                        <div className='container-imagen'>
+                                        <img src ={prod.skin} alt=""  className='img-item'/> 
+                                        </div>
+                                        <div className='container-button'>
+                                        <button className='btn-button-item'>
+                                            detalle del producto
+                                        </button>
+                                        </div>
+                                    </div>    
+                                </div>)
+        }
        </section>
     )
 }
--- React-eCommerce/src/componentes/Items/ItemList.jsx
+++
@@ -1,9 +0,0 @@
-/* const getFetch = new Promise ((resolve)=>{
-    resolve (productList)
-})
-
-const Item = ()=> {
- getFetch
- .then (respuesta => console.log (respuesta))
- .catch ((err)=> console.log (err))
- .finally(()=>console.log ("promesa finalizada")) */
