f8802c7194fbdc8989f44d196fe011060c54f633
luispino94 <luispinito.torres@gmail.com>
2022-05-16 16:43:49 -0300

    haciendo prueba

---
+++ React-eCommerce/src/componentes/ItemListContainer/ItemDetailContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+const ItemDetailContainer = () => {
+ const [list, SetList] = useState([]);
+
+
+
+
+  
+ 
+    return (
+    <div>ItemDetailContainer</div>
+  )
+}
+
+export default ItemDetailContainer
