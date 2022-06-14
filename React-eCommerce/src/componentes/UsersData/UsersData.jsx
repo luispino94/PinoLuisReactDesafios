01cce915f9f0c6c21f1de2d4cbd471870d2bd8dc
luispino94 <luispinito.torres@gmail.com>
2022-06-14 19:48:58 -0300

    se agrego la db a usersdata

--- React-eCommerce/src/componentes/UsersData/UsersData.jsx
+++ React-eCommerce/src/componentes/UsersData/UsersData.jsx
@@ -3,28 +3,69 @@ import React from 'react'
 import {getAuth} from 'firebase/auth';
 import getFirestoreApp from '../../firebase/config';
 import { useCartContext } from '../Contexto/cartContext';
+import { useState,useEffect } from 'react';
+import { collection, deleteDoc, doc, getDocs, getFirestore } from 'firebase/firestore';
+
 const auth = getAuth(getFirestoreApp());
 
-const userGetInfo = auth.currentUser;
+import './database.scss';
+
 
 const UsersData = () => {
-    // const {user} = useCartContext();
+    const {user} = useCartContext();
+    const [lista, setLista] = useState([]);
 
-    // if (userGetInfo !== null) {
-    //     // The user object has basic properties such as display name, email, etc.
-    //     const userEmail = user.email;
-    //     const userRol = user.rol;
-      
-    //     // The user's ID, unique to the Firebase project. Do NOT use
-    //     // this value to authenticate with your backend server, if
-    //     // you have one. Use User.getToken() instead.
+   //Función es para renderizar la lista de usuarios
+   useEffect(()=>{
+      const db = getFirestore()
+      const getLista = async ()=>{
+        try {
+          const querySnapshot = await getDocs(collection(db,'usuario'))
+          const docs = []
+          querySnapshot.forEach((doc)=>{
+            docs.push({...doc.data(), id:doc.id}) 
+          })
+          setLista (docs)
+        } catch (error) {
+          console.log (error)
+        }
+      }
+      getLista()
+   },[lista]) 
 
-    //   }
-    //  console.log (userEmail); 
-    //  console.log (userRol); 
-    //  console.log (userUid); 
+   //Función para eliminar Usuario de FireStore
+  //  const deleteUser = async (id)=>{
+  //   const db = getFirestore()
+  //   await deleteDoc (doc(db,'usuario', id))
+  //  }
+
   return (
-    <div>UsersData</div>
+    <article className='container-DataBase'>
+      <h2 className='database-title'>Bienvenido a la Data Base de Vulkan</h2>
+      
+      <div className='container-list-db'>
+        <h2 className='database-subtitle'>Lista de Usuarios Registrados</h2>
+            {
+              lista.map(list =>(
+                <div className='card-users-db' key={list.id}>
+                  <p className='correo-title'>Correo: {list.correo}</p>
+                  <p className='correo-title'>Rol: {list.rol}</p>
+                {/* <button className='btn-card-db' onClick={()=>deleteUser(list.id)}>Eliminar</button>
+                <button className='btn-card-db'>Actualizar</button> */}
+                <hr/> 
+                </div>
+              ))
+            }
+        </div>  
+        <div className='container-order-db'>
+          <h2>Acá iría las ordenes realizadas</h2>
+          <h2>Acá iría las ordenes realizadas</h2>
+          <h2>Acá iría las ordenes realizadas</h2>
+          <h2>Acá iría las ordenes realizadas</h2>
+              
+        </div>
+
+    </article>
   )
 }
 
